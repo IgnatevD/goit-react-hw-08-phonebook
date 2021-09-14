3e0ab054ebd6ccad3cfbf36c66e58e1b65eee2a5
IgnatevD <ignatevds@gmail.com>
2021-09-14 20:27:02 +0300

    правки

--- src/servis/contacts-API.js
+++ src/servis/contacts-API.js
@@ -3,14 +3,23 @@
 import axios from "axios";
 axios.defaults.baseURL = "https://connections-api.herokuapp.com";
 
-
+const token = {
+  set(token) {
+    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+  },
+  unset() {
+    axios.defaults.headers.common.Authorization = "";
+  },
+};
 export async function fetchConacts() {
   const { data } = await axios.get(`/contacts`);
+  token.set(data.token);
   return data;
 }
 
 export async function fetchConactsPost(obj) {
   const { data } = await axios.post(`/contacts`, obj);
+  token.set(data.token);
   return data;
 }
 
